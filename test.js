9dbe10f179ed85e491b8cfb687534a5a0282a787
AndrosGreen <gclandres@gmail.com>
2022-04-24 18:00:28 -0500

    ejercicio terminado

---
+++ test.js
@@ -0,0 +1,9 @@
+/*var contenedor = document.getElementById("main");
+
+function addText (){
+    var texto = document.createElement('h1');
+    texto.innerText = "Prueba";
+    contenedor.append(texto);
+}
+
+setTimeout(addText, 1000);*/
